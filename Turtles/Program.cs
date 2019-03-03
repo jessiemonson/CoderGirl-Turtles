cba65b21b957b0bfca2c1646695bb37bdbf9d138
Jessie Monson <jessiemonson1980@gmail.com>
2019-03-02 18:18:57 -0600

    final turtle.

--- Turtles/Program.cs
+++ Turtles/Program.cs
@@ -6,6 +6,10 @@ namespace Turtles
     {
         public static void Main()
         {
+            int counter = 1;
+            while (counter <= 1000)
+                Console.WriteLine($"I like turtles");
+            counter++;
             // TODO: Prints "I like turtles." 1000 times.
 
             Console.ReadLine();
